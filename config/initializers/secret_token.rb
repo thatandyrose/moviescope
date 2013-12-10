737bacf9b033149c226f0de4d69cc52d6a6107df
andy rose <thatandyrose@gmail.com>
2013-12-10 20:03:22 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moviescope::Application.config.secret_token = '67092a35bcece3d93a3e6975ff7a5c46c9c0d637dac7c52918f5aac79e8e0343fed5253b9644ca39501475e8f8c34bbaf690efd9f3e575969d35ec915f9ea562'
